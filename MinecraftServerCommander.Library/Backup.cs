e646ad2e48d898d8f0d792ffa2de33eb4eff9762
F16Gaming <adam.hellberg95@gmail.com>
2011-02-13 17:16:42 +0100

    Added skeleton for the backup class.

---
+++ MinecraftServerCommander.Library/Backup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftServerCommander.Library
+{
+	public class Backup
+	{
+		public string Date { get; private set; }
+		public string File { get; private set; }
+
+		public Backup(string date, string file)
+		{
+			Date = date;
+			File = file;
+		}
+
+		public void Restore()
+		{
+			
+		}
+	}
+}
