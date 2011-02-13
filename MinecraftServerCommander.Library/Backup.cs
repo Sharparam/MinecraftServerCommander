70c0cc260366e7affea986a6dc1a6e9e92035abf
F16Gaming <adam.hellberg95@gmail.com>
2011-02-13 17:18:44 +0100

    Added some comments.

--- MinecraftServerCommander.Library/Backup.cs
+++ MinecraftServerCommander.Library/Backup.cs
@@ -7,15 +7,24 @@ namespace MinecraftServerCommander.Library
 {
 	public class Backup
 	{
+		/// <summary>
+		/// The date (dd-MM-yyyy) this backup was created.
+		/// </summary>
 		public string Date { get; private set; }
-		public string File { get; private set; }
+		/// <summary>
+		/// Name of the backup (will be foldername).
+		/// </summary>
+		public string Name { get; private set; }
 
-		public Backup(string date, string file)
+		public Backup(string date, string name)
 		{
 			Date = date;
-			File = file;
+			Name = name;
 		}
 
+		/// <summary>
+		/// Restore this backup, doing this will overwrite the current world.
+		/// </summary>
 		public void Restore()
 		{
 			
