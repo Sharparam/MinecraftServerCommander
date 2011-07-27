605dae1648b23ca21cb9f6032a6938f21dddedea
F16Gaming <adam.hellberg95@gmail.com>
2011-07-27 05:22:43 +0200

    Updated items.txt with 1.7 blocks and items.

--- MinecraftServerCommander.GUI/Properties/AssemblyInfo.cs
+++ MinecraftServerCommander.GUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.21.0407")]
+[assembly: AssemblyVersion("2.2.22.2026")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
